16f321172dca19477e4c2edc50e440e47af8bbb4
Wyzp <muriloonita@live.com>
2017-10-10 00:06:10 -0300

    Revert " cpu_pm/rt: replace rt rwlock with raw spinlock"
    This reverts commit 167fd67a100ffd50d171b3da2f88ebc676f3f754.

--- kernel/cpu_pm.c
+++ kernel/cpu_pm.c
@@ -22,9 +22,7 @@
 #include <linux/spinlock.h>
 #include <linux/syscore_ops.h>
 
-bool from_suspend = false;
-
-static DEFINE_RAW_SPINLOCK(cpu_pm_notifier_lock);
+static DEFINE_RWLOCK(cpu_pm_notifier_lock);
 static RAW_NOTIFIER_HEAD(cpu_pm_notifier_chain);
 
 static int cpu_pm_notify(enum cpu_pm_event event, int nr_to_call, int *nr_calls)
@@ -52,9 +50,9 @@ int cpu_pm_register_notifier(struct notifier_block *nb)
 	unsigned long flags;
 	int ret;
 
-	raw_spin_lock_irqsave(&cpu_pm_notifier_lock, flags);
+	write_lock_irqsave(&cpu_pm_notifier_lock, flags);
 	ret = raw_notifier_chain_register(&cpu_pm_notifier_chain, nb);
-	raw_spin_unlock_irqrestore(&cpu_pm_notifier_lock, flags);
+	write_unlock_irqrestore(&cpu_pm_notifier_lock, flags);
 
 	return ret;
 }
@@ -74,9 +72,9 @@ int cpu_pm_unregister_notifier(struct notifier_block *nb)
 	unsigned long flags;
 	int ret;
 
-	raw_spin_lock_irqsave(&cpu_pm_notifier_lock, flags);
+	write_lock_irqsave(&cpu_pm_notifier_lock, flags);
 	ret = raw_notifier_chain_unregister(&cpu_pm_notifier_chain, nb);
-	raw_spin_unlock_irqrestore(&cpu_pm_notifier_lock, flags);
+	write_unlock_irqrestore(&cpu_pm_notifier_lock, flags);
 
 	return ret;
 }
@@ -102,15 +100,15 @@ int cpu_pm_enter(void)
 	int nr_calls;
 	int ret = 0;
 
-	raw_spin_lock(&cpu_pm_notifier_lock);
-	ret = cpu_pm_notify(CPU_PM_ENTER, -1, &nr_calls, NULL);
+	read_lock(&cpu_pm_notifier_lock);
+	ret = cpu_pm_notify(CPU_PM_ENTER, -1, &nr_calls);
 	if (ret)
 		/*
 		 * Inform listeners (nr_calls - 1) about failure of CPU PM
 		 * PM entry who are notified earlier to prepare for it.
 		 */
-		cpu_pm_notify(CPU_PM_ENTER_FAILED, nr_calls - 1, NULL, NULL);
-	raw_spin_unlock(&cpu_pm_notifier_lock);
+		cpu_pm_notify(CPU_PM_ENTER_FAILED, nr_calls - 1, NULL);
+	read_unlock(&cpu_pm_notifier_lock);
 
 	return ret;
 }
@@ -132,9 +130,9 @@ int cpu_pm_exit(void)
 {
 	int ret;
 
-	raw_spin_lock(&cpu_pm_notifier_lock);
-	ret = cpu_pm_notify(CPU_PM_EXIT, -1, NULL, NULL);
-	raw_spin_unlock(&cpu_pm_notifier_lock);
+	read_lock(&cpu_pm_notifier_lock);
+	ret = cpu_pm_notify(CPU_PM_EXIT, -1, NULL);
+	read_unlock(&cpu_pm_notifier_lock);
 
 	return ret;
 }
@@ -161,17 +159,15 @@ int cpu_cluster_pm_enter(void)
 	int nr_calls;
 	int ret = 0;
 
-	raw_spin_lock(&cpu_pm_notifier_lock);
-	ret = cpu_pm_notify(CPU_CLUSTER_PM_ENTER, -1, &nr_calls,
-			(void *) aff_level);
+	read_lock(&cpu_pm_notifier_lock);
+	ret = cpu_pm_notify(CPU_CLUSTER_PM_ENTER, -1, &nr_calls);
 	if (ret)
 		/*
 		 * Inform listeners (nr_calls - 1) about failure of CPU cluster
 		 * PM entry who are notified earlier to prepare for it.
 		 */
-		cpu_pm_notify(CPU_CLUSTER_PM_ENTER_FAILED, nr_calls - 1, NULL,
-				(void *) aff_level);
-	raw_spin_unlock(&cpu_pm_notifier_lock);
+		cpu_pm_notify(CPU_CLUSTER_PM_ENTER_FAILED, nr_calls - 1, NULL);
+	read_unlock(&cpu_pm_notifier_lock);
 
 	return ret;
 }
@@ -196,9 +192,9 @@ int cpu_cluster_pm_exit(void)
 {
 	int ret;
 
-	raw_spin_lock(&cpu_pm_notifier_lock);
-	ret = cpu_pm_notify(CPU_CLUSTER_PM_EXIT, -1, NULL, (void *) aff_level);
-	raw_spin_unlock(&cpu_pm_notifier_lock);
+	read_lock(&cpu_pm_notifier_lock);
+	ret = cpu_pm_notify(CPU_CLUSTER_PM_EXIT, -1, NULL);
+	read_unlock(&cpu_pm_notifier_lock);
 
 	return ret;
 }
